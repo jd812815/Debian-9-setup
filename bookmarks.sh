b0f751290361c1678da9970dcc94298092ca102a
jd812815 <77907449+jd812815@users.noreply.github.com>
2021-01-25 05:30:44 -0500

    Update bookmarks.sh

--- bookmarks.sh
+++ bookmarks.sh
@@ -8,6 +8,7 @@ https://www.unrealircd.org/docs/Cron_job
 
 XMPP:
 https://docs.ejabberd.im/admin/installation/#install-on-linux
+https://www.ejabberd.im/forum/28654/user-registration/index.html
 
 TOR:
 https://medium.com/@ajphillips/how-to-create-your-own-tor-hidden-service-436bece8602f
