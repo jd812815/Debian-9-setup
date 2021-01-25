98399fe19e679dee1214d04d3ba0b66839b8ac30
jd812815 <77907449+jd812815@users.noreply.github.com>
2021-01-25 03:22:09 -0500

    Update bookmarks.sh

--- bookmarks.sh
+++ bookmarks.sh
@@ -3,5 +3,5 @@ https://vitux.com/debian-apache/
 https://www.digitalocean.com/community/tutorials/how-to-create-a-self-signed-ssl-certificate-for-apache-in-debian-9
 
 UnrealIRCd:
-https://steamforge.net/wiki/index.php/How_to_Setup_an_UnrealIRCd_Server_on_Debian_9
+https://www.unrealircd.org/docs/Installing_from_source#Creating_a_configuration_file
 https://www.unrealircd.org/docs/Cron_job
